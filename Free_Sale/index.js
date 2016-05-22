d756d81ee38912986fb1df5b0cc7fbc0545a9312
naveenrajceg13 <naveenrajp.erode@gmail.com>
2016-05-22 01:02:58 -0500

    latest

--- Free_Sale/index.js
+++ Free_Sale/index.js
@@ -1,6 +1,14 @@
 var express=require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var database = require('./config/database'); 			// load the database config
+var morgan = require('morgan'); 		// log requests to the console (express4)
+var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var app=express();
 var handlebar="";
+
+mongoose.connect(database.url);
 handlebars=require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
@@ -9,21 +17,15 @@ app.use('/node_modules',express.static(__dirname+'/node_modules'));
 
 app.set('port',process.env.PORT||3000);
 app.use(express.static(__dirname+'/public'));
+app.use(morgan('dev')); 										// log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-app.get('/',function(req,res){
-    res.render('home');
-});
 
-app.get('/sale',function(req,res){
-    res.render('sale');
-});
+require('./app/routes.js')(app);
 
-app.get('/contact',function(req,res){
-    res.render('contact');
-});
-app.get('/Post_Sale',function(req,res){
-    res.render('Post_Sale');
-});
 app.listen(app.get('port'),function(){
    console.log('listening in port'); 
 }); 
