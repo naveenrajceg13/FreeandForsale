7f77b18fbfa21e796d3ee313b32c54e81d50ebbb
jithendhiralal <jithendhir91@gmail.com>
2016-05-23 01:09:14 -0500

    commit

---
+++ Free_Sale/App/routes.js
@@ -0,0 +1,79 @@
+var Todo = require('./models/todo');
+
+module.exports = function(app) {
+
+	// api ---------------------------------------------------------------------
+	// get all todos
+	app.get('/api/todos', function(req, res) {
+
+		// use mongoose to get all todos in the database
+		Todo.find(function(err, todos) {
+
+			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+			if (err)
+				res.send(err)
+            
+			res.json(todos); // return all todos in JSON format
+		});
+	});
+
+	// create todo and send back all todos after creation
+	app.post('/api/todos', function(req, res) {
+
+		// create a todo, information comes from AJAX request from Angular
+		Todo.create({
+			text : req.body.text,
+            text1: req.body.text1,
+			done : false
+		}, function(err, todo) {
+			if (err)
+				res.send(err);
+
+			// get and return all the todos after you create another
+			Todo.find(function(err, todos) {
+				if (err)
+					res.send(err)
+                console.log(todos);
+				res.json(todos);
+			});
+		});
+
+	});
+
+	// delete a todo
+	app.delete('/api/todos/:todo_id', function(req, res) {
+		Todo.remove({
+			_id : req.params.todo_id
+		}, function(err, todo) {
+			if (err)
+				res.send(err);
+
+			// get and return all the todos after you create another
+			Todo.find(function(err, todos) {
+				if (err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
+
+	// application -------------------------------------------------------------
+	
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.get('/sale',function(req,res){
+    res.render('sale');
+});
+
+app.get('/contact',function(req,res){
+    res.render('contact');
+});
+app.get('/Post_Sale',function(req,res){
+    res.render('Post_Sale');
+});
+    app.get('/profile',function(req,res){
+    res.render('profile');
+});
+};
